f86fc37ebaeb1b7d1f8a5ac5b59bf689827f3f04
Andy Wendt <andy@awendt.com>
2019-04-22 00:04:48 -0600

    Add selection sort notes

--- src/MinimumSwaps.java
+++ src/MinimumSwaps.java
@@ -2,9 +2,18 @@ public class MinimumSwaps {
     static int minimumSwaps(int[] arr) {
         int length = arr.length;
         int swaps = 0;
+        //
+        // Selection sort
+        //
 
+        // iterate through the array to sort the array
+        // from smallest to largest
         for (int i = 0; i < length; i++) {
+            // establish the minimum as the current iteration
             int min = i;
+
+            // run through the array starting
+            // from the outer loop plus one
             for (int j = i + 1; j < length; j++) {
                 if (arr[j] < arr[min]) min = j;
             }
