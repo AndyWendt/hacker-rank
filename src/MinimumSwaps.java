d12f7fa909f7ed2702e39229f965a34a392cb5b2
Andy Wendt <andy@awendt.com>
2019-04-22 00:05:56 -0600

    Add selection sort notes

--- src/MinimumSwaps.java
+++ src/MinimumSwaps.java
@@ -15,9 +15,11 @@ public class MinimumSwaps {
             // run through the array starting
             // from the outer loop plus one
             for (int j = i + 1; j < length; j++) {
+                // the current min is greater than j
                 if (arr[j] < arr[min]) min = j;
             }
 
+            // make the swap if min is less than the current array
             if (arr[min] < arr[i]) {
                 int temp = arr[i];
                 arr[i] = arr[min];
