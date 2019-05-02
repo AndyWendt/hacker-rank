b5f4ccd12d67a244faea6531a5d5084babbb2022
Andy Wendt <andy@awendt.com>
2019-05-01 22:50:36 -0600

    Implement min swaps faster

--- src/MinimumSwaps.java
+++ src/MinimumSwaps.java
@@ -1,31 +1,39 @@
+import java.util.ArrayList;
+
 public class MinimumSwaps {
     static int minimumSwaps(int[] arr) {
         int length = arr.length;
         int swaps = 0;
-        //
-        // Selection sort
-        //
 
-        // iterate through the array to sort the array
-        // from smallest to largest
         for (int i = 0; i < length; i++) {
-            // establish the minimum as the current iteration
-            int min = i;
+            // loop through the array and find the correct position for the
+            // current element and swap it
 
-            // run through the array starting
-            // from the outer loop plus one
-            for (int j = i + 1; j < length; j++) {
-                // the current min is greater than j
-                if (arr[j] < arr[min]) min = j;
-            }
-
-            // make the swap if min is less than the current array
-            if (arr[min] < arr[i]) {
-                int temp = arr[i];
-                arr[i] = arr[min];
-                arr[min] = temp;
+            if (arr[i] != i + 1) {
+                int correctKey = arr[i] - 1;
+                int currentTemp = arr[i];
+                int swapTemp = arr[correctKey];
+                arr[correctKey] = currentTemp;
+                arr[i] = swapTemp;
+                int currentI = i;
                 swaps++;
+
+                if ((swapTemp - 1) != i) {
+                    while (swapTemp < (i + 1)) {
+                        correctKey = swapTemp - 1;
+                        swapTemp = arr[correctKey];
+                        currentTemp = arr[currentI];
+                        arr[currentI] = swapTemp;
+                        arr[correctKey] = currentTemp;
+                        swaps++;
+
+                        if ((swapTemp - 1) == currentI) {
+                            break;
+                        }
+                    }
+                }
             }
+
         }
 
         return swaps;
--- tests/MinimumSwapsTest.java
+++ tests/MinimumSwapsTest.java
@@ -2,6 +2,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,11 +21,39 @@ class MinimumSwapsTest {
         assertSame(expectedSwaps, result);
     }
 
+    @ParameterizedTest
+    @MethodSource("swapsFileDataProvider")
+    public void it_calculates_the_minimum_swaps_from_files(int expectedSwaps, String fileName) throws IOException {
+        int[] input = readBlocksFromFile(fileName);
+        int result = MinimumSwaps.minimumSwaps(input);
+        assertEquals(expectedSwaps, result);
+    }
+
+
     static Stream<Arguments> swapsDataProvider() {
         return Stream.of(
                 // expected, input int[]
                 Arguments.arguments(3, new int[] {2, 3, 4, 1, 5}),
-                Arguments.arguments(3, new int[] {1, 3, 5, 2, 4, 6, 7})
+                Arguments.arguments(3, new int[] {1, 3, 5, 2, 4, 6, 7}),
+                Arguments.arguments(5, new int[] {7, 1, 3, 2, 4, 5, 6})
         );
+    }
+
+    static Stream<Arguments> swapsFileDataProvider() {
+        return Stream.of(
+                // expected, input int[]
+                Arguments.arguments(99987, "input12.txt")
+        );
+    }
+
+    private int[] readBlocksFromFile(String file) throws IOException {
+        URL url = getClass().getResource("data/" + file);
+        Path path = Paths.get(url.getPath());
+        Stream<String> stream = Files.lines(path);
+        Iterator<String> iterator = stream.iterator();
+        iterator.next();
+        String currentLineString = iterator.next().trim();
+        String[] numbers = currentLineString.split("\\s+");
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
     }
 }
